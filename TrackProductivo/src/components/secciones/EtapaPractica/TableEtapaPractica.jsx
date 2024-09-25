df9b7742a63f85882784b8c0b1b95a79829c4a33
Molina44 <jorgenunez4m@gmail.com>
2024-09-24 21:53:35 -0500

    se arreglo el error que se tenia al actualizar una empresa

--- TrackProductivo/src/components/secciones/EtapaPractica/TableEtapaPractica.jsx
+++ TrackProductivo/src/components/secciones/EtapaPractica/TableEtapaPractica.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-//import registerFichas from '../components/secciones/Fichas/registerFichas';
 import GlobalTable from '../../componets_globals/GlobalTable';
-//import updateFichas from '../components/secciones/Fichas/updateFichas';
-//import deleteFichas from '../components/secciones/Fichas/deleteFichas'; // Asegúrate de importar correctamente el componente DeleteArea
-import CardComponent from '../../CardComponent';
-
-
 function TableEtapaPractica() {
   const columns = [
     'codigo',
@@ -25,13 +19,9 @@ function TableEtapaPractica() {
       <main className='w-full p-3 h-screen'>
         <div className='my-5 flex flex-col py-5'>
 
-          <CardComponent title="Fichas Registradas"  />
-          {/* <RegisterArea /> */}
           <GlobalTable 
             columns={columns} 
             dataEndpoint="/fichas/listar" 
-            //updateComponent={UpdateArea} 
-            //deleteComponent={DeleteArea} 
           />
         </div>
 
